63a7b12d74b8dde559335a0ff1cab7b81b3c0166
Osorkon21 <Osorkon21@users.noreply.github.com>
2023-11-20 00:04:59 -0600

    Add main handlebars page and some CSS

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,4 +3,14 @@ const { User, Article, Comment } = require("../models");
 
 // write handlebars HTML? Or write routes?
 
+router.get("/", async (req, res) => {
+  try {
+    res.render("home");
+  }
+  catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+})
+
 module.exports = router;
